82b8b3a9137329c08c8df811bc2e4610f4de2dbb
7Mason7 <masoncilento@gmail.com>
2025-04-07 19:06:04 -0500

    Modified Account.py > order_check

--- src/backtester/Account.py
+++ src/backtester/Account.py
@@ -41,10 +41,10 @@ class Account:
         """
         Fetches the current balance and positions value combined.
         """
-        _total_position_value: float = 0
+        total_position_value: float = 0
         for key in self.holdings:
-            _total_position_value = _total_position_value + self.holdings[key] # * fetch_price(key)    
-        return _total_position_value + self.cash
+            total_position_value = total_position_value + self.holdings[key] # * fetch_price(key)    
+        return total_position_value + self.cash
 
 
     def order_create(self, order: Order):
@@ -66,34 +66,43 @@ class Account:
         self.open_orders.append(order)
 
 
-    def order_cancel(self, order_index):
+    def order_cancel(self, order_index: int):
         """
         Cancels a given order at the selected index within the open_orders attribute.
+
+        Parameters
+        ----------
+        order_index : int, required
+            The index of hte order within the Account.open_orders list.
         """
         del self.open_orders[order_index]
 
 
-    def order_check(self, current_price):
+    def order_check(self, current_price: float):
         """
         Pass a price to check all open_orders for execution. If the order is executed, the order is removed and added to activity.
+
+        Parameters
+        ----------
+        current_price : float, required
+            The price you want to check the order against.
         """
         i = 0
-        while i < len(self.open_orders):
-            order = self.open_orders[i]
+        executed_orders = []
+        for order in self.open_orders:
             order.check_fill(current_price)
             if order.status == "Executed":
+                executed_orders.append(order)
                 if order.buy_or_sell == "Buy":
-                    _total_cost = order.executed_price * order.quantity
-                    self.cash = self.cash - _total_cost
-                    self.activity.append(-_total_cost)
-                    self.holdings[order.symbol] = order.quantity
+                    total_cost = order.executed_price * order.quantity
+                    self.cash -= total_cost
+                    self.activity.append(-total_cost)
+                    self.holdings[order.symbol] = self.holdings.get(order.symbol, 0) + order.quantity
                 elif order.buy_or_sell == "Sell":
-                    _total_proceeds = (order.executed_price * order.quantity)
-                    self.cash = self.cash + _total_proceeds
-                    self.activity.append(_total_proceeds)
-                    self.holdings[order.symbol] = -order.quantity
+                    total_proceeds = (order.executed_price * order.quantity)
+                    self.cash += total_proceeds
+                    self.activity.append(total_proceeds)
+                    self.holdings[order.symbol] = self.holdings.get(order.symbol, 0) - order.quantity
                 del self.open_orders[i]
-            else:
-                i += 1
-        else:
-            i += 1
+        for order in executed_orders:
+            self.open_orders.remove(order)
