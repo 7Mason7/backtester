a7740528b27daf7049c4c090696141130e373533
7Mason7 <masoncilento@gmail.com>
2025-06-03 17:47:58 -0500

    Modified backtest_account

--- src/backtester/brokerage/backtest_account.py
+++ src/backtester/brokerage/backtest_account.py
@@ -59,7 +59,7 @@ class BacktestMarginAccount(Account):
 
     def get_open_order_cost(self, price_dict: dict[str, float]) -> float:
         """
-        Gets the total open order cost within the OMS.
+        Gets the total open order cost within the Order Management System (OMS).
         """
         order_cost = 0
         for order in self.oms.open_orders.values():
@@ -74,13 +74,13 @@ class BacktestMarginAccount(Account):
                     order_cost += order.quantity * price_dict[order.symbol]
         return order_cost
 
-    def get_open_order_req(self, price_dict: dict[str : float]) -> float:
+    def get_open_order_maint_req(self, price_dict: dict[str : float]) -> float:
         """
-        Gets the total open order marign requirements within the OMS.
+        Gets the total open order maintenance requirements within the Order Management System (OMS).
         """
         order_req = 0
         for order in self.oms.open_orders.values():
-            if order.direction == "buy":
+            if order.direction == "buy" and order.short == False:
                 if isinstance(order, MarketOrder):
                     order_req += 0.25 * price_dict[order.symbol] * order.quantity
                 elif isinstance(order, LimitOrder):
@@ -94,6 +94,28 @@ class BacktestMarginAccount(Account):
                     order_req += 0.3 * order.limit_price * order.quantity
                 elif isinstance(order, StopOrder):
                     order_req += 0.3 * order.stop_price * order.quantity
+        return order_req
+    
+    def get_open_order_initial_req(self, price_dict: dict[str : float]) -> float:
+        """
+        Gets the total open order initial margin requirements within the Order Management System (OMS).
+        """
+        order_req = 0
+        for order in self.oms.open_orders.values():
+            if order.direction == "buy" and order.short == False:
+                if isinstance(order, MarketOrder):
+                    order_req += self.margin_requirements['initial_long'] * price_dict[order.symbol] * order.quantity
+                elif isinstance(order, LimitOrder):
+                    order_req += self.margin_requirements['initial_long'] * order.limit_price * order.quantity
+                elif isinstance(order, StopOrder):
+                    order_req += self.margin_requirements['initial_long'] * order.stop_price * order.quantity
+            elif order.direction == "sell" and order.short == True:
+                if isinstance(order, MarketOrder):
+                    order_req += self.margin_requirements['initial_short'] * price_dict[order.symbol] * order.quantity
+                elif isinstance(order, LimitOrder):
+                    order_req += self.margin_requirements['initial_short'] * order.limit_price * order.quantity
+                elif isinstance(order, StopOrder):
+                    order_req += self.margin_requirements['initial_short'] * order.stop_price * order.quantity
         return order_req
 
     def get_equity(self, price_dict: dict[str : float]) -> float:
@@ -114,8 +136,3 @@ class BacktestMarginAccount(Account):
     def get_maintenance_excess(self, price_dict: dict[str : float]) -> float:
         return self.get_equity(price_dict) - self.get_maintenance_requirement(price_dict)
     
-    def get_buying_power(self, price_dict: dict[str : float], short = False) -> float:
-        if short == False:
-            return min(self.get_maintenance_excess(price_dict) / 0.25, 0.5 * self.get_equity(price_dict))
-        else:
-            return min(self.get_maintenance_excess(price_dict) / 0.3, 0.5 * self.get_equity(price_dict))
