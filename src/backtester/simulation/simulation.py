a166e458f74788f24dd8457293c196c80bde8d2b
7Mason7 <masoncilento@gmail.com>
2025-06-04 18:17:02 -0500

    Added simulation outline

--- src/backtester/simulation/simulation.py
+++ src/backtester/simulation/simulation.py
@@ -1,5 +1,17 @@
+import pandas as pd
 
 class Simulation:
     def __init__(self):
-        self.candle_data: list
-        
+        self.candle_data: pd.DataFrame
+    
+    
+    # Attributes: 
+    #   Pandas Dataframe of each security, normalized so the indicies match by timestamp.
+    #   Price dictionary. For each security time series, create a price dictionary and update per tick.
+    #   Your account
+    #   Steps = The number of intervals within the price df
+    # Methods:
+    #   Step forward
+    #   Return price data
+    #   Place an order
+
